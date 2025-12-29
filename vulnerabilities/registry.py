4c13c3f19af0ea30d4063dcb4d416b694490c9aa
0xMarturano <anthony.marturano@protonmail.com>
2025-12-29 16:06:17 -0500

    Create registry.py

---
+++ vulnerabilities/registry.py
@@ -0,0 +1,9 @@
+from vulnerabilities.shellshock_2014 import SHELLSHOCK
+from vulnerabilities.bluekeep_2019 import BLUEKEEP
+from vulnerabilities.eternalblue_2017 import ETERNALBLUE
+
+VULNERABILITIES = [
+    SHELLSHOCK,
+    BLUEKEEP,
+    ETERNALBLUE
+]
